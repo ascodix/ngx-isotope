eebcdbec932fe7acf6d1b607237f184cec9401b5
Albuisson Stéphane <stephane.albuisson@gmail.com>
2017-08-24 23:29:47 +0200

    Ajout de l'enum correspondant à la liste des layouts possibles pour la librairie Isotope

---
+++ src/models/layout-modes.enum.ts
@@ -0,0 +1,11 @@
+export enum LayoutModes {
+  MASONRY = <any>'masonry',
+  FIT_ROWS = <any>'fitRows',
+  CELLS_BY_ROW = <any>'cellsByRow',
+  VERTICAL = <any>'vertical',
+  PACKERY = <any>'packery',
+  MASONRY_HORIZONTAL = <any>'masonryHorizontal',
+  FIT_COLUMNS = <any>'fitColumns',
+  CELLS_BY_COLUMN = <any>'cellsByColumn',
+  HORIZ = <any>'horiz'
+}
